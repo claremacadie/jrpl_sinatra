0c09fd87ef81240bb4453503b4b698d68389c905
Clare MacAdie <clare@macadie.co.uk>
2022-06-08 12:43:43 +0100

    Added login methods in database_persistence.rb copied from Bookle.

--- database_persistence.rb
+++ database_persistence.rb
@@ -14,6 +14,50 @@ class DatabasePersistence
     @db.close
   end
 
+  def upload_new_user_credentials(user_name, password)
+    hashed_password = BCrypt::Password.create(password).to_s
+    sql = 'INSERT INTO users (name, password) VALUES ($1, $2)'
+    query(sql, user_name, hashed_password)
+  end
+
+  def change_username_and_password(old_username, new_username, new_password)
+    hashed_password = BCrypt::Password.create(new_password).to_s
+    sql = 'UPDATE users SET name = $1, password = $2 WHERE name = $3'
+    query(sql, new_username, hashed_password, old_username)
+  end
+
+  def change_username(old_username, new_username)
+    sql = 'UPDATE users SET name = $1 WHERE name = $2'
+    query(sql, new_username, old_username)
+  end
+
+  def change_password(old_username, new_password)
+    hashed_password = BCrypt::Password.create(new_password).to_s
+    sql = 'UPDATE users SET password = $1 WHERE name = $2'
+    query(sql, hashed_password, old_username)
+  end
+
+  def reset_password(username)
+    new_password = BCrypt::Password.create('bookle').to_s
+    sql = 'UPDATE users SET password = $1 WHERE name = $2'
+    query(sql, new_password, username)
+  end
+
+  def load_user_credentials
+    sql = 'SELECT name, password FROM users'
+    result = query(sql)
+
+    result.each_with_object({}) do |tuple, hash|
+      hash[tuple['name']] = tuple['password']
+    end
+  end
+
+  def user_id(user_name)
+    sql = 'SELECT id FROM users WHERE name = $1'
+    result = query(sql, user_name)
+    result.first['id'].to_i
+  end
+
   def all_users_list
     sql = <<~SQL
       SELECT
