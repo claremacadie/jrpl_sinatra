ad4f011d1b8dd6f3ef9608aee009002a5d83d5d6
Clare MacAdie <clare@macadie.co.uk>
2022-06-12 17:39:23 +0100

    Added user_digned_in? check to require_signed_in_as_admin.

--- jrpl.rb
+++ jrpl.rb
@@ -55,7 +55,7 @@ def user_signed_in?
 end
 
 def require_signed_in_as_admin
-  return if session[:user_role] == 'Admin'
+  return if user_signed_in? && session[:user_role] == ('Admin')
   session[:intended_route] = request.path_info
   session[:message] = 'You must be an administrator to do that.'
   redirect '/'
