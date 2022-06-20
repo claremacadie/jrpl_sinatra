855b181b04629c928380d96850b608a01b103798
Clare MacAdie <clare@macadie.co.uk>
2022-06-20 11:30:26 +0100

    Added select all jquery functionality for tournament stages on match filter form.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,3 +30,12 @@ $(function() {
     }
   });
 });
+
+$("form.filter_form").ready(function() {
+  $('#select-all').click(function() {
+      var checked = this.checked;
+      $('input[type="checkbox"]').each(function() {
+      this.checked = checked;
+  });
+  })
+});
