23255083fef36cf7039939f777366827c1a4f33c
Clare MacAdie <clare@macadie.co.uk>
2022-06-22 23:35:24 +0100

    Alphabetised database_persistence.rb.

--- database_persistence.rb
+++ database_persistence.rb
@@ -32,13 +32,13 @@ class DatabasePersistence
 
   private
 
-  def query(statement, *params)
-    @logger.info "#{statement}: #{params}"
-    @db.exec_params(statement, params)
-  end
-
   def convert_str_to_int(str)
     # This is needed because nil.to_i returns 0!!!
     str ? str.to_i : nil
+  end
+
+  def query(statement, *params)
+    @logger.info "#{statement}: #{params}"
+    @db.exec_params(statement, params)
   end
 end
