73d47fdc4807411834a873ef1fb2013f8c86d5ed
Clare MacAdie <clare@macadie.co.uk>
2022-06-22 16:20:51 +0100

    Added test for autoquiz scoreboard after adding result.

--- test/jrpl_test.rb
+++ test/jrpl_test.rb
@@ -2032,5 +2032,17 @@ def test_filter_matches_group_stages_and_final
     assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
     assert_includes last_response.body, 'Scoreboard'
     assert_includes last_response.body.gsub(/\n/, ''), '<td>Clare Mac</td>        <td>2</td>        <td>4</td>        <td>6</td>'
+    refute_includes last_response.body.gsub(/\n/, ''), '<td>Mr. Median</td>        <td>2</td>        <td>4</td>        <td>6</td>'
+  end
+  
+  def test_autoquiz_scoreboard_add_result
+    post '/match/add_result', {match_id: 6, home_pts: '2', away_pts: '1'}, admin_session
+
+    get '/toggle_scoring_system', {scoring_system: 'autoquiz'}, user_11_session
+    assert_equal 200, last_response.status
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+    assert_includes last_response.body, 'Scoreboard'
+    assert_includes last_response.body.gsub(/\n/, ''), '<td>Clare Mac</td>        <td>2</td>        <td>4</td>        <td>6</td>'
+    assert_includes last_response.body.gsub(/\n/, ''), '<td>Mr. Median</td>        <td>2</td>        <td>4</td>        <td>6</td>'
   end
 end
