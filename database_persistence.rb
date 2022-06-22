9d9e903ed790d11b5f4bc897d753a2c9cb43a4a2
Clare MacAdie <clare@macadie.co.uk>
2022-06-22 19:18:12 +0100

    Created login_db_pers module.

--- database_persistence.rb
+++ database_persistence.rb
@@ -1,6 +1,10 @@
 require 'pg'
 
+require_relative 'login_db_pers'
+
 class DatabasePersistence
+  include LoginDBPers
+  
   def initialize(logger)
     @db = if ENV['RACK_ENV'] == 'test'
             PG.connect(dbname: 'jrpl_test')
@@ -12,122 +16,6 @@ class DatabasePersistence
 
   def disconnect
     @db.close
-  end
-
-  def upload_new_user_credentials(user_details)
-    hashed_pword = BCrypt::Password.create(user_details[:pword]).to_s
-    sql = 'INSERT INTO users (user_name, email, pword) VALUES ($1, $2, $3)'
-    query(sql, user_details[:user_name], user_details[:email], hashed_pword)
-  end
-
-  def change_username(old_user_name, new_user_name)
-    sql = 'UPDATE users SET user_name = $1 WHERE user_name = $2'
-    query(sql, new_user_name, old_user_name)
-  end
-
-  def change_pword(old_user_name, new_pword)
-    hashed_pword = BCrypt::Password.create(new_pword).to_s
-    sql = 'UPDATE users SET pword = $1 WHERE user_name = $2'
-    query(sql, hashed_pword, old_user_name)
-  end
-
-  def change_email(old_user_name, new_email)
-    sql = 'UPDATE users SET email = $1 WHERE user_name = $2'
-    query(sql, new_email, old_user_name)
-  end
-
-  def reset_pword(username)
-    new_pword = BCrypt::Password.create('jrpl').to_s
-    sql = 'UPDATE users SET pword = $1 WHERE user_name = $2'
-    query(sql, new_pword, username)
-  end
-
-  def load_user_credentials
-    sql = 'SELECT user_name, pword, email FROM users'
-    result = query(sql)
-
-    result.each_with_object({}) do |tuple, hash|
-      hash[tuple['user_name']] =
-        { pword: tuple['pword'], email: tuple['email'] }
-    end
-  end
-
-  def user_id(user_name)
-    sql = 'SELECT user_id FROM users WHERE user_name = $1'
-    result = query(sql, user_name)
-    result.first['user_id'].to_i
-  end
-
-  def user_id_from_cookies(series_id, token)
-    sql = 'SELECT user_id FROM remember_me WHERE series_id = $1 AND token = $2;'
-    result = query(sql, series_id, token)
-    return nil if result.ntuples == 0
-    result.first['user_id'].to_i
-  end
-
-  def user_name_from_email(email)
-    sql = 'SELECT user_name FROM users WHERE email = $1'
-    result = query(sql, email)
-    return nil if result.ntuples == 0
-    result.first['user_name']
-  end
-
-  def load_user_details(user_id)
-    sql = select_query_single_user
-    result = query(sql, user_id)
-    return nil if result.ntuples == 0
-    result.map do |tuple|
-      tuple_to_users_details_hash(tuple)
-    end.first
-  end
-
-  def load_all_users_details
-    sql = select_query_users_details()
-    result = query(sql)
-    result.map do |tuple|
-      tuple_to_users_details_hash(tuple)
-    end
-  end
-
-  def user_admin?(user_id)
-    sql = 'SELECT * FROM user_role WHERE user_id = $1 AND role_id = $2;'
-    result = query(sql, user_id, admin_id())
-    !(result.ntuples == 0)
-  end
-
-  def assign_admin(user_id)
-    sql = 'INSERT INTO user_role VALUES ($1, $2);'
-    query(sql, user_id, admin_id())
-  end
-
-  def unassign_admin(user_id)
-    sql = 'DELETE FROM user_role WHERE user_id = $1 AND role_id = $2;'
-    query(sql, user_id, admin_id())
-  end
-
-  def series_id_list
-    sql = 'SELECT series_id FROM remember_me;'
-    result = query(sql)
-    return [] if result.ntuples == 0
-    result.map { |tuple| tuple['series_id'] }
-  end
-
-  def save_cookie_data(user_id, series_id_value, token_value)
-    sql = 'INSERT INTO remember_me VALUES ($1, $2, $3, $4);'
-    query(sql, user_id, series_id_value, token_value, Time.now)
-  end
-
-  def save_new_token(user_id, series_id_value, token_value)
-    sql = <<~SQL
-      UPDATE remember_me SET token = $1, date_added = $2
-      WHERE user_id = $3 AND series_id = $4;
-    SQL
-    query(sql, token_value, Time.now, user_id, series_id_value)
-  end
-
-  def delete_cookie_data(series_id, token)
-    sql = 'DELETE FROM remember_me WHERE series_id = $1 AND token = $2;'
-    query(sql, series_id, token)
   end
 
   def load_all_matches(user_id)
--- jrpl.rb
+++ jrpl.rb
@@ -8,9 +8,13 @@ require 'tilt/erubis'
 require_relative 'database_persistence'
 require_relative 'loginable'
 require_relative 'jrpl_route_helpers'
+require_relative 'jrpl_view_helpers'
 
 include Loginable
 include RouteHelpers
+
+# Constant definitions
+LOCKDOWN_BUFFER = 30 * 60 # 30 minutes
 
 configure do
   enable :sessions
@@ -30,93 +34,10 @@ after do
   @storage.disconnect
 end
 
-# Constant definitions
-LOCKDOWN_BUFFER = 30 * 60 # 30 minutes
-
-# Helper methods for view templates
-# rubocop:disable Metrics/BlockLength
+# View helper methods
 helpers do
-  def home_team_name(match)
-    if match[:home_team_name].nil?
-      match[:home_tournament_role]
-    else
-      match[:home_team_name]
-    end
-  end
-
-  def away_team_name(match)
-    if match[:away_team_name].nil?
-      match[:away_tournament_role]
-    else
-      match[:away_team_name]
-    end
-  end
-
-  def home_team_prediction(match)
-    prediction = match[:home_team_prediction]
-    if prediction.nil?
-      'no prediction'
-    else
-      prediction
-    end
-  end
-
-  def away_team_prediction(match)
-    prediction = match[:away_team_prediction]
-    if prediction.nil?
-      'no prediction'
-    else
-      prediction
-    end
-  end
-
-  def home_team_points(match)
-    if match[:home_pts].nil?
-      'no result'
-    else
-      match[:home_pts]
-    end
-  end
-
-  def away_team_points(match)
-    if match[:away_pts].nil?
-      'no result'
-    else
-      match[:away_pts]
-    end
-  end
-
-  def match_locked_down?(match)
-    match_date_time = "#{match[:match_date]} #{match[:kick_off]}"
-    (Time.now + LOCKDOWN_BUFFER) > Time.parse(match_date_time)
-  end
-
-  def previous_match(match_id)
-    match_list = load_match_list()
-    current_match_index = match_list.index { |match| match == match_id }
-    current_match_index = 1 if current_match_index.nil?
-    previous_match_index = current_match_index - 1
-    if previous_match_index < 0
-      nil
-    else
-      match_list[previous_match_index]
-    end
-  end
-
-  def next_match(match_id)
-    match_list = load_match_list()
-    max_index = match_list.size - 1
-    current_match_index = match_list.index { |match| match == match_id }
-    current_match_index = 1 if current_match_index.nil?
-    next_match_index = current_match_index + 1
-    if next_match_index > max_index
-      nil
-    else
-      match_list[next_match_index]
-    end
-  end
+  include ViewHelpers
 end
-# rubocop:enable Metrics/BlockLength
 
 # Routes
 get '/' do
