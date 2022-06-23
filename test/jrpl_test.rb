9e8a07aa1a5c251fbdc4d381f9fc8a0f2903c34d
Clare MacAdie <clare@macadie.co.uk>
2022-06-23 14:24:31 +0100

    Created TestPredictions module.

--- test/jrpl_test.rb
+++ test/jrpl_test.rb
@@ -7,6 +7,7 @@ require 'simplecov'
 require_relative '../jrpl'
 require_relative 'jrpl_test_login'
 require_relative 'jrpl_test_matches_filter'
+require_relative 'jrpl_test_predictions'
 require_relative 'jrpl_test_scoreboard'
 require_relative 'jrpl_test_view_match'
 
@@ -16,6 +17,7 @@ class CMSTest < Minitest::Test
   include Rack::Test::Methods
   include TestLogin
   include TestMatchesFilter
+  include TestPredictions
   include TestScoreboard
   include TestViewMatch
 
@@ -146,54 +148,6 @@ class CMSTest < Minitest::Test
 
     assert_equal 302, last_response.status
     assert_equal 'You must be signed in to do that.', session[:message]
-  end
-  
-  def test_add_new_prediction
-    post '/match/add_prediction', {match_id: '12', home_team_prediction: '98', away_team_prediction: '99'}, user_11_session
-    
-    assert_equal 302, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_equal 'Prediction submitted.', session[:message]
-    
-    get last_response['Location']
-    assert_includes last_response.body, '98'
-    assert_includes last_response.body, '99'
-  end
-  
-  def test_change_prediction
-    post '/match/add_prediction', {match_id: '11', home_team_prediction: '98', away_team_prediction: '99'}, user_11_session
-    
-    assert_equal 302, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_equal 'Prediction submitted.', session[:message]
-    
-    get last_response['Location']
-    assert_includes last_response.body, '98'
-    assert_includes last_response.body, '99'
-  end
-  
-  def test_add_decimal_prediction
-    post '/match/add_prediction', {match_id: '11', home_team_prediction: '2.3', away_team_prediction: '3'}, user_11_session_with_all_criteria
-    
-    assert_equal 422, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_includes last_response.body, 'Your predictions must be integers.'
-  end
-  
-  def test_add_negative_prediction
-    post '/match/add_prediction', {match_id: '11', home_team_prediction: '-2', away_team_prediction: '3'}, user_11_session_with_all_criteria
-    
-    assert_equal 422, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_includes last_response.body, 'Your predictions must be non-negative.'
-  end
-  
-  def test_add_prediction_lockeddown_match
-    post '/match/add_prediction', {match_id: '1', home_team_prediction: '2', away_team_prediction: '3'}, user_11_session_with_all_criteria
-    
-    assert_equal 422, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_includes last_response.body, 'You cannot add or change your prediction because this match is already locked down!'
   end
   
   def test_add_new_result
